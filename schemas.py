d9b90e1c2b6af4072c59d8bbf7ffed072858b366
yasinskydew <yasinsky2015@gmail.com>
2026-04-17 17:32:11 +0300

    Implement update and delete operations for posts and users in FastAPI. Introduce new schemas for user and post updates, enhance error handling for user and post modifications, and update the database model to support cascading deletes. Add a new dependency for aiosqlite in pyproject.toml.

--- schemas.py
+++ schemas.py
@@ -16,10 +16,18 @@ class UserResponse(UserBase):
     image_file: str | None = None
     image_path: str
 
+class UserUpdate(BaseModel):
+    username: str | None = Field(default=None, min_length=1, max_length=50)
+    email: EmailStr | None = Field(default=None, max_length=120)
+    image_file: str | None = Field(default=None)
 
 class PostBase(BaseModel):
     title: str = Field(min_length=1, max_length=100)
     content: str = Field(min_length=1)
+
+class PostUpdate(BaseModel):
+    title: str | None= Field(default=None, min_length=1, max_length=100)
+    content: str | None= Field(default=None, min_length=1)
 
 class PostCreate(PostBase):
     user_id: int
